50b9fc76b33ac74067d68019f3d4a0d867ea9856
kokeshi <koki21345589@icloud.com>
2024-06-26 12:18:57 +0900

    work5終了

--- work5.py
+++ work5.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 
 # ↓↓↓ お約束のコード ↓↓↓
 window = tk.Tk()
@@ -9,30 +10,47 @@ fg_color = "#ffffff"  # 白
 window.configure(bg=bg_color)
 # ↑↑↑ お約束のコード ↑↑↑
 
+a = [
+    "tkinter",
+    "geometry",
+    "widgets",
+    "messagebox",
+    "configure",
+    "label",
+    "column",
+    "rowspan",
+    "grid",
+    "init",
+]
+
 
 def button_action1():
-    num = str((entry1.get()))
-    if num == "aiueo":
+    num = entry1.get().strip()
+    if num in a:
         label1.config(text="正解！")
+        update_random_word()
+        entry1.delete(0, "end")
     else:
         label1.config(text="不正解！")
+
+
+def update_random_word():
+    random_word = random.choice(a)
+    label1.config(text=random_word)
 
 
 # 出力ラベルの作成
 label1 = tk.Label(window, text="aiueo", bg=bg_color, fg=fg_color)
 label1.pack(pady=10)
+update_random_word()
 
 # 入力フィールドの作成
-entry1 = tk.Entry(text="aiueo", bg=fg_color, fg=bg_color)
+entry1 = tk.Entry(window, bg=fg_color, fg=bg_color)
 entry1.pack(pady=10)
 
 # ボタンの作成
 button1 = tk.Button(window, text="OK", command=button_action1)
 button1.pack(pady=10)
-
-# button1 = tk.Button(window, text="-", command=button_action2)
-# button1.pack(pady=10)
-
 
 # ↓↓↓ お約束のコード ↓↓↓
 window.mainloop()
